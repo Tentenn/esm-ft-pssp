3f896868ea6f4442ef59917a88334b19517f5921
Kok Yuan Tang <tang@cip.ifi.lmu.de>
2023-02-25 22:10:38 +0100

    implement peft

--- pssp_finetune.py
+++ pssp_finetune.py
@@ -9,8 +9,10 @@ from evaluate import load
 import numpy as np
 from pathlib import Path
 import torch
+from torch import nn
 import re
 import json
+from peft import LoraConfig, get_peft_model
 
 """
 Finetune ESM2 on Netsurf2.0 Secondary Structure Dataset
@@ -18,11 +20,12 @@ Finetune ESM2 on Netsurf2.0 Secondary Structure Dataset
 class PSSPFinetuner:
     def __init__(self):
         self.model_checkpoint = "facebook/esm2_t12_35M_UR50D"
+        # self.model_checkpoint = "facebook/esm2_t6_8M_UR50D"
         self.device = torch.device("cuda")
 
     def run(self):
-        train_sequences, train_labels = get_jsonl_data(Path("data/train.jsonl"))
-        test_sequences, test_labels = get_jsonl_data(Path("data/val.jsonl"))
+        train_sequences, train_labels = get_jsonl_data(Path("data/train_filter_no_256.jsonl"))
+        test_sequences, test_labels = get_jsonl_data(Path("data/val_filter_no_256.jsonl"))
 
         tokenizer = AutoTokenizer.from_pretrained(self.model_checkpoint)
 
@@ -37,7 +40,39 @@ class PSSPFinetuner:
 
         num_labels = 3
         model = AutoModelForTokenClassification.from_pretrained(self.model_checkpoint, num_labels=num_labels)
-        model = model.to("cuda")
+
+        # print(model)
+
+        ## applying PEFT
+        for param in model.parameters():
+            param.requires_grad = False  # freeze the model - train adapters later
+            if param.ndim == 1:
+                # cast the small parameters (e.g. layernorm) to fp32 for stability
+                param.data = param.data.to(torch.float32)
+        # model.gradient_checkpointing_enable()  # reduce number of stored activations
+        model.enable_input_require_grads()
+
+        #class CastOutputToFloat(nn.Sequential):
+        #    def forward(self, x): return super().forward(x).to(torch.float32)
+
+        #model.lm_head = CastOutputToFloat(model.lm_head)
+
+        from peft import LoraConfig, get_peft_model
+
+        config = LoraConfig(
+            r=16,
+            lora_alpha=32,
+            target_modules=["dense"],
+            lora_dropout=0.05,
+            bias="none",
+            task_type="TOKEN_CLASSIFICATION"
+        )
+
+        model = get_peft_model(model, config)
+        print_trainable_parameters(model)
+        # print("success")
+
+        model = model#.to("cuda")
         data_collator = DataCollatorForTokenClassification(tokenizer)
         model_name = self.model_checkpoint.split("/")[-1]
         batch_size = 8
@@ -84,7 +119,19 @@ class PSSPFinetuner:
         access_token = Path('hf_token').read_text()
         # model = model.to("cuda")
 
-
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
 
 def get_jsonl_data(jsonlfile: Path):
     """
